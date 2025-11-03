435b4f82a708e26e5904dda4447131edfa51a4f4
KOALAS2648 <Koalak1d361@gmail.com>
2025-11-03 10:10:14 +0000

    yoly

--- timeCofBS.py
+++ timeCofBS.py
@@ -1,8 +1,30 @@
+import matplotlib.pyplot as plt
 import random as r
 
 l = r.randint(0, 999999)
-print(f"start length of l is: {l}")
-while True:
-    input()
+x =[]
+y= []
+n=0
+while l != 0:
+    
     l //=2
-    print(l)
+    
+    x.append(l)
+    y.append(n)
+    n+=1
+# x axis values
+
+# corresponding y axis values
+
+# plotting the points 
+plt.plot(x, y)
+# naming the x axis
+plt.xlabel('x - axis')
+# naming the y axis
+plt.ylabel('y - axis')
+
+# giving a title to my graph
+plt.title('My first graph!')
+
+# function to show the plot
+plt.show()
