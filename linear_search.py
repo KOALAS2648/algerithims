78b5ffb29e16d22ef0e825778d793258176f44a7
KOALAS2648 <Koalak1d361@gmail.com>
2025-11-04 10:15:02 +0000

    g

--- linear_search.py
+++ linear_search.py
@@ -2,7 +2,7 @@ def Linear_search(l:list, target:any, n=0):
     n = 0
     if n > len(l):
         return False
-    for i in range(len(l)):
+    for _ in range(len(l)):
         if l[n] == target:
             return n
         n +=1
