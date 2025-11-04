06994fc11409d34781bf86b65af1b94f6e05d44f
KOALAS2648 <Koalak1d361@gmail.com>
2025-11-04 15:50:00 +0000

    timer sorted out

--- timeSearches.py
+++ timeSearches.py
@@ -3,33 +3,47 @@ from lp import *
 from binary_search import Binary_search
 from linear_search import Linear_search
 import time as t
+import os
 
+find_val = 5118
+TIMES = 1000
+print(f"trying to find:{find_val}")
 
-find_val = 768
 def binary_search_time(sum=0):
-    for _ in range(0,4):
-        start_time = t.time()
+    done_times = 0
+    for _ in range(0,TIMES+1):
+        start_time = t.perf_counter()
         b = Binary_search(n, find_val)
-        end_time = t.time()
+        end_time = t.perf_counter()
         full_time = end_time-start_time
         sum += full_time
-    return sum /3
-
-time = binary_search_time()
+        print("binary serach")
+        print(f"amount done:{(done_times/TIMES)*100:.1f}%")
+        done_times +=1
+        os.system("clear")
+        
+    return sum /TIMES
+b=[]
+timeBS = binary_search_time()
 file = open("times/binarySearchtimes.txt", "a")
-file.write(f"{time:.29f},\n")
-print(f"binary search: {time:.29f}")
+file.write(f"{timeBS:.29f}: averaged over {TIMES},\n ")
 
 def linear_search_time(sum=0):
-    for _ in range(0,4):
-        start_time = t.time()
+    done_times = 0
+    for _ in range(0,TIMES+1):
+        start_time = t.perf_counter()
         v  = Linear_search(n, find_val)
-        end_time = t.time()
+        end_time = t.perf_counter()
         full_time = end_time-start_time
-        sum += start_time
-    return sum /3
+        sum += full_time
+        print("linear serach")
+        print(f"amount done:{(done_times/TIMES)*100:.1f}%")
+        done_times +=1
+        os.system("clear")
+    return sum /TIMES
 
 time = linear_search_time()
 file = open("times/linear_times.txt", "a")
-file.write(f"{time:.29f},\n")
-print(f"linear search: {time:.29f}")
+file.write(f"{time:.29f}: averaged over {TIMES},\n")
+print(f"binary search: {timeBS:.29f}: averaged over {TIMES} times")
+print(f"linear search: {time:.29f}: averaged over {TIMES} times")
